470bd2374c363b2b07d0cd7c7a0b8866cafdf14b
LivingProgram <kyhan316@gmail.com>
2018-03-03 15:39:50 -0500

    add chosen hyperparameters

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -184,7 +184,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 100
-learning_rate = 0.1
-hidden_nodes = 2
+iterations = 1600
+learning_rate = 1
+hidden_nodes = 7
 output_nodes = 1
